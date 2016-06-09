ba7c73cafff0354808834fd47937d0ab3b122cce
NomShin <shin.nomu@gmail.com>
2016-06-09 16:51:19 +0900

    データクラス追加

---
+++ Hakuturu/Hakuturu/Models/PcInfoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;   //追加
+using System.ComponentModel.DataAnnotations;   //追加
+
+namespace Hakuturu.Models
+{
+    public class PcInfoItem
+    {
+        [Display(Name = "PC管理番号")]
+        public string PcId { set; get; }
+        [Display(Name = "IPアドレス")]
+        public string IpAddress { set; get; }
+        [Display(Name = "使用区分")]
+        public string UseSegment { set; get; }
+        [Display(Name = "機種")]
+        public string ModelName { set; get; }
+        [Display(Name = "型番")]
+        public string ModelNo { set; get; }
+        [Display(Name = "使用者名")]
+        public string UserName { set; get; }
+        [Display(Name = "PC名")]
+        public string PcName { set; get; }
+        [Display(Name = "備考")]
+        public string Remarks { set; get; }
+
+    }
+    public class PcInfoDBContext : DbContext
+    {
+        public DbSet<PcInfoItem> PcInfoList { set; get; }
+    }
+}
